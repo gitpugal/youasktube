ccfb0c75915a1f16ccb857041cf9f7e1b0a082b6
unknown <pugalarasan2014@gmail.com>
2025-05-28 10:02:51 +0530

    initial commit

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     try {
       const callbackUrl =
         process.env.NODE_ENV === "production"
-          ? "http://localhost:3000/new-chat"
+          ? "http://youasktube.info/new-chat"
           : "http://localhost:3000/new-chat";
 
       const response = await signIn("google", {
