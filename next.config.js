56f2dffaa004e7b0bb48f8b597de63c25d61a105
unknown <pugalarasan2014@gmail.com>
2025-05-29 15:43:40 +0530

    change: dockefile

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+    reactStrictMode: false,
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+        unoptimized: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 module.exports = nextConfig;
